b415454d189e3351f8c624cb97858f4029ffa1bc
manasb-uoe <manas.bajaj94@gmail.com>
2020-06-14 20:23:25 +0100

    updated decorated logger

--- manasb/libs/decoratedlogger/src/lib/decoratedlogger.tsx
+++ manasb/libs/decoratedlogger/src/lib/decoratedlogger.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Anotherlogger } from '@manasb/anotherlogger';
 
 import './decoratedlogger.css';
 
 /* eslint-disable-next-line */
-export interface DecoratedloggerProps {}
+export interface DecoratedloggerProps { }
 
 export const Decoratedlogger = (props: DecoratedloggerProps) => {
+  useEffect(() => {
+    console.log("this is decorated");
+  }, []);
+
   return (
     <div>
       <h1>Welcome to decoratedlogger!</h1>
